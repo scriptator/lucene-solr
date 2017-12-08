d2e847fd7121e96a51f228ad47d97bbdfb0447f1
Johannes Vass <johannes.vass@gmail.com>
2017-12-08 20:33:01 +0100

    Add dummy subclass BM25ModSimilarity

---
+++ lucene/core/src/java/org/apache/lucene/search/similarities/BM25ModSimilarity.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.lucene.search.similarities;
+
+/**
+ * Modification of the classical BM25 similarity measure:
+ *
+ * TODO
+ * Your task is to modify the score calculation in this BM25Similarity class
+ * and create a new similarity class called BM25ModSimilarity. You then use it
+ * in your index by modifying the configurations.
+ *
+ * for 15 points: Lv, Yuanhua, and ChengXiang Zhai. ”When documents are very long, bm25 fails!.” Proceedings of SIGIR 2011. link
+ * for 30 points: Lipani, Aldo, et al. ”Verboseness Fission for BM25 Document Length Normal- ization.” Proceedings of ICTIR 2015. link
+ */
+public class BM25ModSimilarity extends BM25Similarity {
+
+  public BM25ModSimilarity(float k1, float b) {
+    super(k1, b);
+  }
+
+  public BM25ModSimilarity() {
+  }
+
+}
